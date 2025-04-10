138e19890cf0d76f0e461cccef680b822e970a5a
CJ-KE <chegejames867@gmail.com>
2025-04-10 20:41:54 +0300

    list, appending exercise

---
+++ list.py
@@ -0,0 +1,27 @@
+# empty list
+my_list = []
+
+# Append 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert 15 
+my_list.insert(1, 15)
+
+#Extend the list with [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+#Remove the last element from the list
+my_list.pop()  
+
+#Sort the list in ascending order
+my_list.sort()
+
+#Find and print the index of the value 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+#final version of the list
+print("Final list:", my_list)
